ca891e44c5987e82c665ac5cc37adad9f1aff120
Ryan Havens <ryan.d.havens@gmail.com>
2016-12-10 21:38:38 -0500

    Working fingerprint database with mongo

--- server.py
+++ server.py
@@ -1,8 +1,19 @@
 from flask import Flask, request, send_from_directory
+# from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.pymongo import PyMongo
+
 import json
 import pprint
+import os
+import datetime
 
 app = Flask(__name__, static_url_path='/', static_folder='')
+mongo = PyMongo(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost/fingerprinting"
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['CSRF_ENABLED'] = True
+# app.config['SECRET_KEY'] = "mingchow" # todo load from environ?
+# db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET'])
 def index():
@@ -15,9 +26,30 @@ def send_js(path):
 @app.route('/store_fingerprint', methods=['POST'])
 def store_fingerprint():
     content = request.get_json(silent=True, force=True)
-    print content
-    print json.loads(request.data)
-    print pprint.pformat(request.environ, depth=5)
+    fingerprint = content['fingerprint']
+    user = mongo.db.users.find({'fingerprint':fingerprint})
+    n_records = user.count()
+    print n_records
+    if n_records:
+        if (n_records > 1):
+            print 'Multiple users with this fingerprint %s' % fingerprint
+        else:
+            print 'Found matching fingerprint for this user %s' % fingerprint
+    else:
+        print 'New fingerprint %s' % fingerprint
+
+        to_insert = {
+                'fingerprint':fingerprint,
+                'components':json.dumps(content['components']),
+                'created_at':datetime.datetime.utcnow(),
+                'updated_at':datetime.datetime.utcnow()
+        }
+
+        returned_id = mongo.db.users.insert_one(to_insert)
+
+    # print content
+
+
     return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
 if __name__ == '__main__':
